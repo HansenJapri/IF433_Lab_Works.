eff018b1d43c02bd53e2b3f820011819d66217a0
Hansen Japri <hansenj5206@gmail.com>
2026-02-13 10:17:11 +0700

    week02: add warning validation in init block

--- src/oop_00000108259_HansenJapri/week02/student.kt
+++ src/oop_00000108259_HansenJapri/week02/student.kt
@@ -7,3 +7,22 @@ class Student (
 ){
     // Body class kosong dulu
 }
+
+class Student (val name: String, val nim: String, var major: String) {
+
+    init {
+        // Validasi Sederhana: Cek panjang NIM
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
+
+    // Secondary Constructor
+    // Wajib memanggil Primary Constructor menggunakan 'this()'
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+}
