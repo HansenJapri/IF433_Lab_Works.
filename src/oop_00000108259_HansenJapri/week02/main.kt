9029ca3350d3cfd18eb811389361d00f176d28bf
Hansen Japri <hansenj5206@gmail.com>
2026-02-13 10:23:38 +0700

    week02: add secondary constructor

--- src/oop_00000108259_HansenJapri/week02/main.kt
+++ src/oop_00000108259_HansenJapri/week02/main.kt
@@ -1,6 +1,15 @@
-//package oop_001_johnthor.week02
+package oop_001_johnthor.week02
 
 import java.util.Scanner
+
+// Class didefinisikan di luar main
+class Student(val name: String, val nim: String, var major: String) {
+
+    // LANGKAH 4: Secondary Constructor berada di dalam body class
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+}
 
 fun main() {
     val scanner = Scanner(System.`in`)
@@ -13,18 +22,21 @@ fun main() {
     print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
 
-    scanner.nextLine() // Bersihkan buffer newline (Penyakit klasik Scanner!)
+    scanner.nextLine() // Bersihkan buffer
 
-    // Validasi di sisi pemanggil (Main)
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-        // Program berhenti di sini untuk mahasiswa ini, tidak membuat objek
     } else {
-        print("Masukkan Jurusan: ")
+        print("Masukkan Jurusan (Kosongkan jika belum ada): ")
         val major = scanner.nextLine()
 
-        // Instansiasi Objek karena data sudah aman
-        val s1 = Student(name, nim, major)
+        if (major.isEmpty()) {
+            // Memanggil Secondary Constructor
+            val s1 = Student(name, nim)
+        } else {
+            // Memanggil Primary Constructor
+            val s1 = Student(name, nim, major)
+        }
         println("Status: Pendaftaran Selesai.")
     }
 }
