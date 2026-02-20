a98120fe60669ff66a8f9bfd2eca76c973c2ffa6
Hansen Japri <hansenj5206@gmail.com>
2026-02-20 11:38:43 +0700

    week03: fix setter using backing field

--- src/oop_00000108259_HansenJapri/week03/employee.kt
+++ src/oop_00000108259_HansenJapri/week03/employee.kt
@@ -2,5 +2,13 @@ package oop_00000108259_HansenJapri.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0 // Gunakan field, bukan this.salary
+            } else {
+                field = value // Gunakan field untuk assign nilai asli
+            }
+        }
 }
 
--- src/oop_00000108259_HansenJapri/week03/main.kt
+++ src/oop_00000108259_HansenJapri/week03/main.kt
@@ -1,4 +1,7 @@
 package oop_00000108259_HansenJapri.week03
 
-class main {
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000 // Ini akan meledakkan program Anda
 }
