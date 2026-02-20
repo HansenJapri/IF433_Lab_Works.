39ec1670e6078a56f822c41468c7a42e1abc061c
Hansen Japri <hansenj5206@gmail.com>
2026-02-20 16:26:16 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000108259_HansenJapri/week03/weaponMain.kt
@@ -0,0 +1,23 @@
+package oop_00000108259_HansenJapri.week03
+
+fun main() {
+    val sword = Weapon("Excalibur")
+    println("--- Mengamankan Senjata: ${sword.name} ---")
+
+    // Inisialisasi awal
+    sword.damage = 100
+    println("Damage Awal: ${sword.damage} (${sword.tier})")
+
+    println("\n[Uji Coba 1: Set Damage ke -50]")
+    sword.damage = -50 // Harus gagal dan tetap 100
+    println("Damage Sekarang: ${sword.damage}")
+
+    println("\n[Uji Coba 2: Set Damage ke 9999]")
+    sword.damage = 9999 // Harus dipaksa menjadi 1000
+    println("Damage Sekarang: ${sword.damage}")
+
+    println("\n[Hasil Akhir]")
+    println("Weapon: ${sword.name}")
+    println("Final Damage: ${sword.damage}")
+    println("Final Tier: ${sword.tier}")
+}
