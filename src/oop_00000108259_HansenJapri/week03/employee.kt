686103438cf4245525d8fe024d7c61006b0c8a24
Hansen Japri <hansenj5206@gmail.com>
2026-02-20 11:40:55 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000108259_HansenJapri/week03/employee.kt
+++ src/oop_00000108259_HansenJapri/week03/employee.kt
@@ -1,14 +1,34 @@
 package oop_00000108259_HansenJapri.week03
 
 class Employee(val name: String) {
+
+    // 1. Validation dengan Backing Field
     var salary: Int = 0
         set(value) {
             if (value < 0) {
                 println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
-                field = 0 // Gunakan field, bukan this.salary
+                field = 0
             } else {
-                field = value // Gunakan field untuk assign nilai asli
+                field = value
             }
         }
-}
 
+    // 2. Encapsulation: Properti Privat
+    private var performanceRating: Int = 3
+
+    // 3. Computed Property (Pajak 10%)
+    // Properti ini tidak menyimpan data, tapi dihitung setiap kali dipanggil
+    val tax: Double
+        get() = salary * 0.1
+
+    // --- Methods ---
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating, Gaji: $salary, Pajak: $tax")
+    }
+}
