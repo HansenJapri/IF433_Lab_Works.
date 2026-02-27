7585b1bad104c4eeb178bc3010c94d3ae5ec619a
Hansen Japri <hansenj5206@gmail.com>
2026-02-27 10:29:42 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_00000108259_HansenJapri/week04/car.kt
@@ -0,0 +1,12 @@
+package oop_00000108259_HansenJapri.week04
+
+// Car "Is-A" Vehicle. Parameter brand dilempar ke atas melalui Vehicle(brand)
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}
+
+
+
