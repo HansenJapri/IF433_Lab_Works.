99dc03b44152d8bb60710c4bbcbdad6a49270ef1
Hansen Japri <hansenj5206@gmail.com>
2026-03-06 10:26:08 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000108259_HansenJapri/week05/main.kt
+++ src/oop_00000108259_HansenJapri/week05/main.kt
@@ -25,4 +25,19 @@ fun main() {
         }
         println("--------------------------")
     }
+
+
+    class MathHelper {
+        fun hitungLuas(sisi: Int): Int {
+            return sisi * sisi
+        }
+
+        fun hitungLuas(panjang: Int, lebar: Int): Int {
+            return panjang * lebar
+        }
+
+        fun hitungLuas(jariJari: Double): Double {
+            return 3.14 * jariJari * jariJari
+        }
+    }
 }
