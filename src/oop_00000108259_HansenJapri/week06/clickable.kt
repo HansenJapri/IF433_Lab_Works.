2d416c7be215fe4b1a8031ffc31d9ca9385d7c66
Hansen Japri <hansenj5206@gmail.com>
2026-03-13 09:57:39 +0700

    week06: create Button class implementing Clickable

--- src/oop_00000108259_HansenJapri/week06/clickable.kt
+++ src/oop_00000108259_HansenJapri/week06/clickable.kt
@@ -1,11 +1,14 @@
 package oop_00000108259_HansenJapri.week06
 
 interface Clickable {
-    // Properti abstrak (tanpa nilai default)
-    // Kelas yang mengimplementasikan ini wajib memberikan nilai
     val name: String
-
-    // Fungsi abstrak (tanpa body)
     fun click()
+}
+
+
+class Button(override val name: String) : Clickable {
+    override fun click() {
+        println("Tombol '$name' berhasil diklik!")
+    }
 }
 
