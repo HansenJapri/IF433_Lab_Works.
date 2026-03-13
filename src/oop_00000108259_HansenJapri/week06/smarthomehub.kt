82f3af7b6b6481a2491331469c03c9cfbb06debf
Hansen Japri <hansenj5206@gmail.com>
2026-03-13 10:17:52 +0700

    week06: (task) create SmartHomeHub logic

--- src/oop_00000108259_HansenJapri/week06/smarthomehub.kt
+++ src/oop_00000108259_HansenJapri/week06/smarthomehub.kt
@@ -1,2 +1,33 @@
 package oop_00000108259_HansenJapri.week06
 
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("Berhasil menambahkan: ${device.name}")
+    }
+
+    fun turnOffAllSwitches() {
+        println("\n--- Mematikan Semua Perangkat ---")
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff() // Smart Casting otomatis ke Switchable
+            }
+        }
+    }
+
+    fun activateSecurityMode() {
+        println("\n=== MENGAKTIFKAN MODE KEAMANAN ===")
+        for (device in devices) {
+            // Cek jika bisa merekam
+            if (device is Recordable) {
+                device.startRecord()
+            }
+            // Cek jika speaker untuk bunyi sirine
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}
