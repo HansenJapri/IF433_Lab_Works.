18a2107159bece129e77579f0e05bde249beaaa7
Hansen Japri <hansenj5206@gmail.com>
2026-03-13 10:17:25 +0700

    week06: (task) create SmartCCTV with compound interfaces

--- src/oop_00000108259_HansenJapri/week06/smartcctv.kt
+++ src/oop_00000108259_HansenJapri/week06/smartcctv.kt
@@ -1,2 +1,22 @@
 package oop_00000108259_HansenJapri.week06
 
+class SmartCCTV(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable, Recordable {
+
+    override fun turnOn() {
+        println("[$name] Sistem CCTV aktif.")
+        // Otomatis merekam saat dinyalakan sesuai instruksi tugas
+        startRecord()
+    }
+
+    override fun turnOff() {
+        println("[$name] Sistem CCTV non-aktif.")
+        stopRecord()
+    }
+
+    override fun startRecord() {
+        println("[$name] Lensa berputar... Memulai perekaman video...")
+    }
+}
