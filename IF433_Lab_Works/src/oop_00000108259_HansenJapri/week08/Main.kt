9fcd377deec00d0ec54c76ffafef8da9a3631591
Hansen Japri <hansenj5206@gmail.com>
2026-04-17 10:36:32 +0700

    week08: implement chained safe calls and elvis

---
+++ IF433_Lab_Works/src/oop_00000108259_HansenJapri/week08/Main.kt
@@ -0,0 +1,11 @@
+package oop_00000108259_HansenJapri.week08
+
+fun main() {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+
+    println("Tujuan pengiriman: $destination")
+
+}
