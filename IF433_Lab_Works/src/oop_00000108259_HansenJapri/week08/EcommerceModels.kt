e717483542e3c31b72ec63b774c2ca1f17416269
Hansen Japri <hansenj5206@gmail.com>
2026-04-17 10:57:23 +0700

    week08: (task) create sealed class Product and data classes

---
+++ IF433_Lab_Works/src/oop_00000108259_HansenJapri/week08/EcommerceModels.kt
@@ -0,0 +1,9 @@
+package oop_00000108259_HansenJapri.week08
+
+
+sealed class Product
+    data class Electronic(val id: String, val name: String, val warrantyMonths: Int): Product()
+    data class Clothing(val id: String, val name: String, val size: String): Product()
+
+
+
