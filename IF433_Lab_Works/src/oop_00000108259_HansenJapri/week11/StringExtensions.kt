0aa9fc705aa5268becb82b6ea95c2bc0ff00d1a4
Hansen Japri <hansenj5206@gmail.com>
2026-05-08 10:18:31 +0700

    week11: create string extension functions

---
+++ IF433_Lab_Works/src/oop_00000108259_HansenJapri/week11/StringExtensions.kt
@@ -0,0 +1,10 @@
+package oop_00000108259_HansenJapri.week11
+
+fun String.addGreeting(): String {
+    return "Hello $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
+
