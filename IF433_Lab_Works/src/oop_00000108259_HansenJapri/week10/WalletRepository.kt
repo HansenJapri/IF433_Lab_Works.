813a690e5869680353903bbe5c25f0ff36d185d8
Hansen Japri <hansenj5206@gmail.com>
2026-05-14 22:07:39 +0700

    week10: (task) implement generic WalletRepository

---
+++ IF433_Lab_Works/src/oop_00000108259_HansenJapri/week10/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_00000108259_HansenJapri.week10
+
+class WalletRepository<T : Any> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
