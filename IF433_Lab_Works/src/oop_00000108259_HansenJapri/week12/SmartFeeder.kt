3e383d2ba9892227fec8d9c94c8ba0daaf55799c
Hansen Japri <hansenj5206@gmail.com>
2026-05-15 10:24:39 +0700

    week12: (task) test full robust smart feeder pipeline

--- IF433_Lab_Works/src/oop_00000108259_HansenJapri/week12/SmartFeeder.kt
+++ IF433_Lab_Works/src/oop_00000108259_HansenJapri/week12/SmartFeeder.kt
@@ -50,6 +50,9 @@ fun main() {
         println("Peringatan ke Pemilik: ${error.message}")
         println("(Opsional: Berikan chicken jerky secara manual)")
     }
+
+    println("\n=== FINAL STATUS ===")
+    println("Sisa stok akhir: $currentKibbleStock gr")
 }
 
 
