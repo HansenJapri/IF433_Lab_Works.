3288ba2866f8c00692ea538f501bdf6b89358a65
Hansen Japri <hansenj5206@gmail.com>
2026-05-22 21:56:12 +0700

    week13: (task) implement fromCsvTrade with split

--- IF433_Lab_Works/src/oop_00000108259_HansenJapri/week13/CryptoTradeLogger.kt
+++ IF433_Lab_Works/src/oop_00000108259_HansenJapri/week13/CryptoTradeLogger.kt
@@ -3,7 +3,26 @@ package oop_00000108259_HansenJapri.week13
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
 
 
-fun TradeRecord.toCsv(): String {}
+fun TradeRecord.toCsv(): String {
+    return "$id, $symbol, $type, $margin, $pnl"
+}
 
+fun fromCsvTrade(line: String): TradeRecord? {
+    return try {
+
+        val parts = line.split(",")
+
+        TradeRecord(
+            id = parts[0].trim().toInt(),
+            symbol = parts[1].trim(),
+            type = parts[2].trim(),
+            margin = parts[3].trim().toDouble(),
+            pnl = parts[4].trim().toDouble()
+        )
+    } catch (e: Exception) {
+        println("(Log) Data korup diabaikan: $line")
+        null
+    }
+}
 
 
