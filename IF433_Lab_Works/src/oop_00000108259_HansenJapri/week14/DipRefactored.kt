ce0b0fcbc9f0173dba0bc3456588e40536028669
Hansen Japri <hansenj5206@gmail.com>
2026-05-29 10:54:46 +0700

    week14: create Database abstraction and implementations

---
+++ IF433_Lab_Works/src/oop_00000108259_HansenJapri/week14/DipRefactored.kt
@@ -0,0 +1,10 @@
+package oop_00000108259_HansenJapri.week14
+
+interface Database {
+    fun query(sql: String): List<String>
+}
+
+class PostgresDatabase: Database {
+    override fun query(sql: String) = listOf("pg_data1", "pg_data2")
+}
+
